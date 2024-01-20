b264ece16dcf63ab2ed95aa02c5749bd68d2067f
ArtemHul <147180868+ArtemHul@users.noreply.github.com>
2024-01-20 18:31:21 +0100

    1

--- js/myTest.js
+++ js/myTest.js
@@ -1,9 +1,74 @@
-function speed(distance, time) {
-    const maxSpeed =parseFloat (distance / time);
-
-    return `the maximum speed of your car - ${maxSpeed} km/hourse `
-}
-console.log (speed (300, 2.5));
-console.log (speed (800, 4.6));
+// function speed(distance, time) {
+    // const maxSpeed =parseFloat (distance / time);
+// 
+    // return `the maximum speed of your car - ${maxSpeed} km/hourse `
+// }
+// console.log (speed (300, 2.5));
+// console.log (speed (800, 4.6));
 
 //********************************/
+
+// function getSubscriptionPrice(type) {
+ 
+//     switch (type) 
+//       case 'starter' :{
+//      return "0";
+//       break;
+//       case 'professional':
+//      return "20";
+//       break;
+//       case 'organization':
+//       return "50";
+//       break;
+//     default:
+//       return 'Invalid subscription type!';
+//   }
+// }  
+
+// *****************************
+// function isNumberInRange(start, end, number) {
+    // return number >= start && number <= end; }
+// 
+    // console.log (isNumberInRange(10, 30, 17))
+
+    // --------------------
+
+    // function checkAccess(subType) {
+    //     return subType === 'vip' || subType === 'pro';
+    //   }
+    //   console.log (checkAccess ('pro'));
+
+    // *****************************
+// *
+// Функція calculateTotal(number) приймає ціле число (параметр number) 
+// Використовуючи цикл for, доповни код функції так, щоб вона 
+// повертала суму всіх цілих чисел від одиниці до цього числа включно.
+//  Наприклад, якщо number дорівнює 3, то сума - це 1 + 2 + 3, тобто 6.
+// 
+// console.log('Live reload disable.');
+
+// *
+    // function calculateTotal(number) {
+    //     let i = 0;
+    //     for let (2 >= 0; 2 += 1;)
+    //     return number += 1;
+    //   }
+    //   calculateTotal(3)
+
+    // *******************
+//     function calculateTotal(number) {
+//         let sum = 1;
+//         for (let i = 1; i <= 1; i++) {
+//            return sum += i;
+            
+//         }
+
+//     }
+     
+      
+// console.log(calculateTotal(6))
+ let sum = 0;
+ for (let i=0; i<=10; i++){
+    sum += i;
+ }
+ console.log(sum);
